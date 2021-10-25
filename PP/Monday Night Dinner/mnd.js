f2e7a77e64cb6e01c49b64be4b40ef51ffd107e2
Vickie Vien <vickievien@gmail.com>
2021-10-25 10:33:35 -0700

    MND Updates with Lizz

--- PP/Monday Night Dinner/mnd.js
+++ PP/Monday Night Dinner/mnd.js
@@ -1,26 +1,18 @@
-// Each random number RETURNS different restaurant
-
-// When I click this, a RANDOM number will be drawn and will RETURN a restaurant
 
 function randoRestaurant() {
     let random = Math.ceil(Math.random() *5);
     console.log(random);
     if (random === 1) {
-        return '1st restaurant';
+        document.querySelector('.result-output').innerText = '1st restaurant';
     } else if (random === 2) {
-        return '2nd restaurant';
+        document.querySelector('.result-output').innerText = '2nd restaurant';
     } else if (random === 3) {
-        return '3rd restaurant';
+        document.querySelector('.result-output').innerText = '3rd restaurant';
     } else if (random === 4) {
-        return '4th restaurant';
+        document.querySelector('.result-output').innerText = '4th restaurant';
     } else {
-        return '5th restaurant';
+        document.querySelector('.result-output').innerText = '5th restaurant';
     }
 };
 
-console.log(randoRestaurant());
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.img-control').addEventListener('click', randoRestaurant());
-    document.querySelector('.result-input').innerTEXT = randoRestaurant();
-});
+document.querySelector('.img-control').addEventListener('click', randoRestaurant);

1dfeccab4f77915206d06dd72fa4976cbbd067d3
Vickie Vien <vickievien@gmail.com>
2021-10-25 22:12:02 -0700

    Lesson 11 In Class Updates

--- Lesson/Lesson 11/01_bad_intern_plays_chicken/starter_code/js/poultry.js
+++ Lesson/Lesson 11/01_bad_intern_plays_chicken/starter_code/js/poultry.js
@@ -1,16 +1,19 @@
+console.log("hello");
 
 function validateOrderQty(e){
+    e.preventDefault();
 
     // Get the requested quantity
-    const orderQty = document.getElementById('pbeOrderQty').value;
+    let orderQty = document.getElementById('pbeOrderQty').value;
     orderQty = parseInt(orderQty);
 
     // If quantity is above or below the allowed then show alert message
-    if(orderQty < 20){
+    if(orderQty < 10 || orderQty > 20 || isNaN(orderQty)) {
         alert("Please limit your order to a quantity between 10 and 200.");
     }else{
             // Show acceptance alert message if allowed quantity
         alert("Your order has been submitted");
+        document.getElementById('pbeOrderQty').value = "";
     }
 
 
@@ -21,15 +24,16 @@ function validateOrderQty(e){
 
 document.addEventListener('DOMContentLoaded', function(event) {
 
-document.querySelector('#pbeOrderSubmit').addEventListener('click',validateOrder);
+document.querySelector('#pbeOrderSubmit').addEventListener('click',validateOrderQty);
 
-document.querySelector('#pbeDescCharacteristics header a').addEventListener('click',function(e){
+document.querySelector('#pbeDescCharacteristics header').addEventListener('click',function(e){
+    e.preventDefault();
     document.getElementById('pbeDescCharacteristics').classList.toggle('pbeDescAccordionCollapsed');
 });
 
 document.querySelector('#pbeDescHistory header').addEventListener('click',function(e){
     e.preventDefault();
-    document.getElementByClass('pbeDescHistory').classList.toggle('pbeDescAccordionCollapsed');
+    document.getElementById('pbeDescHistory').classList.toggle('pbeDescAccordionCollapsed');
 });
 
 });

5ac159df37e145c66f7e91b6812fc68a7cb0885a
Vickie Vien <vickievien@gmail.com>
2021-11-15 09:04:22 -0800

    update

--- Lesson/Lesson 16_2/01_petfinder/starter_code/js/exercise.js
+++ Lesson/Lesson 16_2/01_petfinder/starter_code/js/exercise.js
@@ -1,48 +1,82 @@
 function selectPet(petType){
+    let thePet = petData[petType];
 
-    // Get the specific pet data set
+    document.querySelector(".petDisplayName").innerHTML = thePet.displayName;
 
-    // Set the title
+    petSpecs = "";
+    petSpecs += `<dt>Size</dt><dd>${thePet.size}</dd>`;
+    petSpecs += `<dt>Weight</dt><dd>${thePet.weight}</dd>`;
+    petSpecs += `<dt>Space Required</dt><dd>${thePet.spaceRequired}</dd>`;
+
+    if (thePet.trainability === true) {
+        petSpecs += `<dt>Trainable</dt><dd><img src ="img/200px-Gnome-emblem-default.svg.png" alt="yes" class="checkmark"></dd>`
+    } else {
+        petSpecs += `<dt>Trainable</dt><dd><img src ="img/200px-Gnome-emblem-unreadable.svg.png" alt="no" class="checkmark"></dd>`
+    };
+
+    if (thePet.fitsOnLap === true) {
+        petSpecs += `<dt>Fits On Lap</dt><dd><img src ="img/200px-Gnome-emblem-default.svg.png" alt="yes" class="checkmark"></dd>`
+    } else {
+        petSpecs += `<dt>Fits On Lap</dt><dd><img src ="img/200px-Gnome-emblem-unreadable.svg.png" alt="no" class="checkmark"></dd>`
+    };
+
+    petSpecs += `<dt>Foods</dt><dd><ul>`;
+        thePet.foods.forEach(n => {
+            petSpecs += `<li>${n}</li>`
+        });
+    petSpecs += `</ul></dd>`;
+    
+    document.querySelector(".petsDataSpecs").innerHTML = petSpecs;
+    
+
+    let babyThumb = "";
+        thePet.images.forEach(n => {
+            babyThumb += `<a href="${n.img}"><img src="${n.thumb}" alt="${n.alt}" class="photoThumb"></a>`
+        });
+    document.querySelector(".thumbHaus").innerHTML = babyThumb;
 
 
+    document.querySelector(".photoHaus").innerHTML = `<img class="photoLarge" alt="${thePet.images[0].alt}" src="${thePet.images[0].img}">` 
 
-    // Empty the specification data
+    function changeBig () {
+        e.preventDefault();
+        console.log('hello');
 
-    // Add the space required, size, weight
- 
-    // Add logic to add the trainability and lap size images
+        // document.querySelector(".photoHaus .photoLarge").setAttribute("src", this.getAttribute("href"));
+        // document.querySelectorAll(".photoHaus .photoLarge").setAttribute("alt", this.getAttribute("alt"));
+        
+        document.querySelector(".thumbHaus").innerHTML = `<a href="${this.getAttribute("img")}"><img src="${this.getAttribute("thumb")}" alt="${this.getAttrbute("alt")}" class="photoLarge"></a>`
+    };
 
+    let allThumbs = document.querySelectorAll(".thumbHaus > a");
+    
+    allThumbs.forEach(n => {
+        n.addEventListener('click', changeBig);
+    });
 
-    // Add the foods (may require loop)
-   
+    let idealSpecs = "";
+        thePet.idealOwner.forEach(n => {
+            idealSpecs += `<li>${n}</li>`;
+        });
+    document.querySelector(".idealFor").innerHTML = idealSpecs;
 
-    // Update the images
-
-  
-
-        // Apply the click event to thumbnails (note - this will likely need to be applied to the a and not the img direclty)
-
-
-    // Empty the ideal for
-
-    // Insert the ideal for
-
-
-    // Empty the Adoption Groups
-
-    // Add the adoption groups
-
-
-}// end selectPet
+    let adoptionSpecs = "";
+        thePet.adoption.forEach(n => {
+            adoptionSpecs += `<li><a href="${n.url}" target="_blank">${n.name}</a></li>`
+        });
+    document.querySelector(".adoptionGroups").innerHTML = adoptionSpecs;
+}
 
 
 
 document.addEventListener('DOMContentLoaded', function(event) {
 
-    // Initial run
     selectPet("dog");
-    //Update whenever a new value is picked
-    document.querySelector(".myClass").addEventListener("change",function(){});
+
+    document.querySelector("#petType").addEventListener("change", function () {
+        let chosenPet = document.querySelector("#petType").value;
+        selectPet(chosenPet);
+    });
 });
 
 

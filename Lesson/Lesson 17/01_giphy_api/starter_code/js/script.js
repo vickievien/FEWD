ff1cca4f4884a1d96a812dadef628c15f6574a98
Vickie Vien <vickievien@gmail.com>
2021-11-17 20:39:03 -0800

    11/17/21

---
+++ Lesson/Lesson 17/01_giphy_api/starter_code/js/script.js
@@ -0,0 +1,45 @@
+
+// ToDo
+// When the user submits the search form, make an API request to Giphy's search endpoint
+// https://api.giphy.com/v1/gifs/search?api_key="YOURAPIKEY"&q="SEARCHSTRING"
+// Display the results in the #giphy-results div provided in the html
+// Each new search should replace the previous search results
+// CSS Bonus: Use flex or grid properties to display the results in a responsive, clean layout
+
+
+
+const apiKey = "0o9UeHyCYFHX9euoFQjUIIHjxSBlkVEh";
+
+async function giphySearch(e) {
+  
+  // prevent the page from submitting/reloading
+  e.preventDefault();
+  
+  // get the user input from the form
+  let searchTerm = document.querySelector("input[name=search-term]").value;
+  searchTerm = searchTerm.replace(/[^0-9A-Za-z ]/gi, '');
+  
+  // make an API call using fetch() - include your API key and the user's search term (template strings are your friend)
+  // async function letsGetData() {
+    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}`;
+    const retrieved = await fetch(apiUrl);
+   
+    // convert your response data into .json()
+    const gotData = await retrieved.json();
+    console.log(gotData);
+  // }
+
+  // clear out all gifs from previous searches
+  const giphyResultDiv = document.querySelector("#giphy-results");
+  giphyResultDiv.innerHTML = "";
+  
+  // use a loop to create and append each image to the dom
+  for(let i=0; i<gotData.data.length; i++) {
+    const newPicTag = document.createElement("img");
+    newPicTag.setAttribute('src',gotData.data[i].images.fixed_width.url);
+    giphyResultDiv.appendChild(newPicTag);
+  }
+}
+
+// dont forget your event listener
+document.querySelector("input[type=submit]").addEventListener('click', giphySearch);
--- Lesson/Lesson16_2/01_petfinder/starter_code/js/exercise.js
+++ Lesson/Lesson16_2/01_petfinder/starter_code/js/exercise.js
@@ -3,7 +3,7 @@ function selectPet(petType){
 
     document.querySelector(".petDisplayName").innerHTML = thePet.displayName;
 
-    petSpecs = "";
+    let petSpecs = "";
     petSpecs += `<dt>Size</dt><dd>${thePet.size}</dd>`;
     petSpecs += `<dt>Weight</dt><dd>${thePet.weight}</dd>`;
     petSpecs += `<dt>Space Required</dt><dd>${thePet.spaceRequired}</dd>`;
@@ -36,16 +36,14 @@ function selectPet(petType){
     document.querySelector(".thumbHaus").innerHTML = babyThumb;
 
 
-    document.querySelector(".photoHaus").innerHTML = `<img class="photoLarge" alt="${thePet.images[0].alt}" src="${thePet.images[0].img}">` 
+    document.querySelector(".photoHaus").innerHTML = `<img class="photoLarge" alt="${thePet.images[0].alt}" src="${thePet.images[0].img}">`;
 
-    function changeBig () {
+    function changeBig (e) {
         e.preventDefault();
         console.log('hello');
 
-        // document.querySelector(".photoHaus .photoLarge").setAttribute("src", this.getAttribute("href"));
-        // document.querySelectorAll(".photoHaus .photoLarge").setAttribute("alt", this.getAttribute("alt"));
-        
-        document.querySelector(".thumbHaus").innerHTML = `<a href="${this.getAttribute("img")}"><img src="${this.getAttribute("thumb")}" alt="${this.getAttrbute("alt")}" class="photoLarge"></a>`
+        document.querySelector(".photoHaus .photoLarge").setAttribute("src", this.getAttribute("href"));
+        document.querySelector(".photoHaus .photoLarge").setAttribute("alt", this.querySelector("img").getAttribute("alt"));
     };
 
     let allThumbs = document.querySelectorAll(".thumbHaus > a");

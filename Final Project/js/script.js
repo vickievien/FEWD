8d6b098f0500cee63842f402a363627448b6409c
Vickie Vien <vickievien@gmail.com>
2021-11-22 17:40:34 -0800

    update

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -1,7 +1,10 @@
 // Toggle Nav Burger
-function toggleBurgerNav () {
+function toggleBurgerNav() {
     let targetNav = document.querySelector('.ww-nav-list').classList.toggle('ww-nav-list-toggle');
 }
 
 let burger = document.querySelector('.ww-nav-burger');
 burger.addEventListener('click', toggleBurgerNav);
+
+
+  

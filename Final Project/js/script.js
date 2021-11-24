0ab56033885000f27ce926d963e0c765c8ddda8f
Vickie Vien <vickievien@gmail.com>
2021-11-23 16:17:00 -0800

    11/23/21 Updates

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -7,4 +7,32 @@ let burger = document.querySelector('.ww-nav-burger');
 burger.addEventListener('click', toggleBurgerNav);
 
 
-  
+// This is for the photo gallery
+let thumbPic = document.querySelectorAll('.thumb-gal-pic');
+console.log(thumbPic);
+
+function applyBig () {
+    document.querySelector('.big-gal-pic').setAttribute('src', this.getAttribute('src'));
+
+    document.querySelector('.big-gal-pic').setAttribute('alt', this.getAttribute('alt'));
+
+    document.querySelector('.big-gal-title').innerHTML = this.getAttribute('alt');
+    
+    document.querySelector('.big-gal-subtitle').innerHTML = this.parentElement.querySelector('.hidden-gal-subtitle').innerText;
+}
+
+thumbPic.forEach(n => {
+    n.addEventListener('click', applyBig);
+});
+
+
+// // This is to validate email form
+// function validateEmail (emailInput) {
+//     let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+//     if (emailInput.value.match(mailFormat)) {
+//         alert('Thank you for subscribing!');
+//         document.ww-
+//         return true;
+//     }
+// }

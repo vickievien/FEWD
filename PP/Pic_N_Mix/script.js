9d031b6e6f85cfe442790c629ca791500d3c5753
Vickie Vien <vickievien@gmail.com>
2021-12-07 19:47:10 -0800

    12/7/21 Update

---
+++ PP/Pic_N_Mix/script.js
@@ -0,0 +1,37 @@
+// document.querySelector(DOMContentLoaded, function(e) {
+//     e.preventDefault();
+
+
+//     function nextSection() {
+//         document.querySelector('.title-page').classList.toggle('noDisplay');
+//         document.querySelector('.pick-candy-section').classList.toggle('noDisplay');
+//     };
+    
+
+
+
+
+//     document.querySelector('.title-page-btn').addEventListener('click', nextSection);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// });
+

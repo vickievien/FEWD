be98df66a7226025b8535ac3c9b18ee6ed1e0585
Vickie Vien <vickie@DESKTOP-A7CACKI.localdomain>
2022-03-25 01:19:42 -0700

    added fonts

--- Private_Practice/urban-jungle/front-end/src/mainIndex/mainIndexComponent.jsx
+++ Private_Practice/urban-jungle/front-end/src/mainIndex/mainIndexComponent.jsx
@@ -5,8 +5,7 @@ import '../App.css';
 const MainIndexComponent = () => {
   return (
     <main class="main-index">
-      
-        
+        <h1 class="main-title-holder">URBAN JUNGLE HOLDER</h1>
     </main>
   );
 }

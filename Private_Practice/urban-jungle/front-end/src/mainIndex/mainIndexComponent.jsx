45b3a569ab84998777809fc04760cf4bd04aaaf3
Vickie Vien <vickie@DESKTOP-A7CACKI.localdomain>
2022-03-25 01:05:40 -0700

    front and back set up. back end data server working

---
+++ Private_Practice/urban-jungle/front-end/src/mainIndex/mainIndexComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+
+const MainIndexComponent = () => {
+  return (
+    <main class="main-index">
+      
+        
+    </main>
+  );
+}
+
+export default MainIndexComponent;
